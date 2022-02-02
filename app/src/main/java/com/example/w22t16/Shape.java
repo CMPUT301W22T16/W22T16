15ab1eb99e318319dabed992177c94ea4230a782
weichen <w3ichen@gmail.com>
2022-02-02 16:05:20 -0700

    Update Shape.java

--- app/src/main/java/com/example/w22t16/Shape.java
+++ app/src/main/java/com/example/w22t16/Shape.java
@@ -3,6 +3,7 @@ package com.example.w22t16;
 public abstract class Shape {
     private int x;
     private int y;
+    String color = "blue";
 
     public Shape(int x, int y) {
         this.x = x;
