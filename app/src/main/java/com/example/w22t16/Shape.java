7e26fe986a05feb1ba326e971803cea1711bdbad
Landyn <landynpugh@gmail.com>
2022-02-02 16:20:23 -0700

    Removed colour from Shape

--- app/src/main/java/com/example/w22t16/Shape.java
+++ app/src/main/java/com/example/w22t16/Shape.java
@@ -3,7 +3,6 @@ package com.example.w22t16;
 public abstract class Shape {
     private int x;
     private int y;
-    String color = "yellow";
 
     public Shape(int x, int y) {
         this.x = x;
