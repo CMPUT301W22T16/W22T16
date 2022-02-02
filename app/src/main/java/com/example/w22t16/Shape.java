06d4540380f1e505a1227bf33b2f27bdab486dcb
Faris Kazmi <faris.kazmi@gmail.com>
2022-02-02 16:11:43 -0700

    Added color to Shape

--- app/src/main/java/com/example/w22t16/Shape.java
+++ app/src/main/java/com/example/w22t16/Shape.java
@@ -3,6 +3,7 @@ package com.example.w22t16;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "yellow";
 
     public Shape(int x, int y) {
         this.x = x;
