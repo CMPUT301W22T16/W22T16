aad354e7cb4d85b493276096a1d37b6e765ff800
Darius Fang <56571687+DarFang@users.noreply.github.com>
2022-02-02 15:52:59 -0700

    addedShape

---
+++ app/src/main/java/com/example/w22t16/Shape.java
@@ -0,0 +1,11 @@
+package com.example.w22t16;
+
+public abstract class Shape {
+    private int x;
+    private int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
