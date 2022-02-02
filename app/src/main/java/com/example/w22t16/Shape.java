2d7df956bb38d097b81a3c344708c1dfa1c033d8
mrcromero <mrcromero33@gmail.com>
2022-02-02 16:15:04 -0700

    Add color field again

--- app/src/main/java/com/example/w22t16/Shape.java
+++ app/src/main/java/com/example/w22t16/Shape.java
@@ -3,10 +3,11 @@ package com.example.w22t16;
 public abstract class Shape {
     private int x;
     private int y;
-    String color = "blue";
+    String color;
 
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
+        this.color = "blue";
     }
 }
