080739dd1bc7e7dfe2b4a287ee84061b1fc6e971
Darius Fang <56571687+DarFang@users.noreply.github.com>
2022-02-02 16:11:57 -0700

    added color

--- app/src/main/java/com/example/w22t16/Shape.java
+++ app/src/main/java/com/example/w22t16/Shape.java
@@ -3,9 +3,10 @@ package com.example.w22t16;
 public abstract class Shape {
     private int x;
     private int y;
-
+    String color;
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
+        this.color = "blue";
     }
 }
